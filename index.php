6a8849624d9769ea97ac3d44f903edc88a6b7454
rcbest <49197270+rcbest@users.noreply.github.com>
2022-11-15 07:15:13 +0300

    Update index.php

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ else
         <h1>Privet Pidoram!</h1>
         <p>Your AWS Elastic Beanstalk <em>PHP</em> application is now running on your own dedicated environment in the JOPA&nbsp;Cloud</p>
         <p>You are running PHP version <?= phpversion() ?></p>
-        <p>This environment is launched with Elastic Beanstalk PHP Platform</p>
+        <p>This environment is launched with Elastic BeanHer PHP Platform</p>
     </section>
 
     <section class="instructions">
